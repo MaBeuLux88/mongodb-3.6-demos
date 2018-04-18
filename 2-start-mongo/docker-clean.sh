c2b90991d98dbb817e46400acea8fd532e11b579
Maxime Beugnet <maxime.beugnet@gmail.com>
2018-04-18 15:40:08 +0200

    simplified the RS docker run script.

--- 2-start-mongo/docker-clean.sh
+++ 2-start-mongo/docker-clean.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 docker stop mongo1 mongo2 mongo3
 docker rm -v mongo1 mongo2 mongo3
-rm -rf data 
+exit 0
--- 2-start-mongo/docker-run.sh
+++ 2-start-mongo/docker-run.sh
@@ -1,22 +1,14 @@
 #!/usr/bin/env bash
 
-mkdir -p data/{db1,db2,db3}
+# Here we start our main MongoDB instances in v3.6
+docker run -d -p 27017:27017 -h mongo1 --network mongonet --network-alias mongo1 --name mongo1 \
+	mongo:3.6.3 --replSet replicaTest --bind_ip_all
 
-# Here we start our main MongoDB instance, in >3.6
-docker run -d -p 27017:27017 -v $(pwd)/data/db1:/data/db1 \
-	-u 1000:1000 -h mongo1 --network mongonet \
-	--network-alias mongo1 --name mongo1 \
-	mongo:3.6.3 --dbpath /data/db1 --replSet replicaTest --bind_ip_all --logpath /data/db1/mongod.log
+docker run -d -p 27018:27017 -h mongo2 --network mongonet --network-alias mongo2 --name mongo2 \
+	mongo:3.6.3 --replSet replicaTest --bind_ip_all
 
-docker run -d -p 27018:27017 -v $(pwd)/data/db2:/data/db2 \
-	-u 1000:1000 -h mongo2 --network mongonet \
-	--network-alias mongo2 --name mongo2 \
-	mongo:3.6.3 --dbpath /data/db2 --replSet replicaTest --bind_ip_all --logpath /data/db2/mongod.log
-
-docker run -d -p 27019:27017 -v $(pwd)/data/db3:/data/db3 \
-	-u 1000:1000 -h mongo3 --network mongonet \
-	--network-alias mongo3 --name mongo3 \
-	mongo:3.6.3 --dbpath /data/db3 --replSet replicaTest --bind_ip_all --logpath /data/db3/mongod.log
+docker run -d -p 27019:27017 -h mongo3 --network mongonet --network-alias mongo3 --name mongo3 \
+	mongo:3.6.3 --replSet replicaTest --bind_ip_all
 
 sleep 1
 
